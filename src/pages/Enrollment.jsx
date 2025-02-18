598af1bd07d6c93c6c9c48ca9fa46b6b244b8b42
Pranav A S <pranavas.22aim@kongu.edu>
2025-02-18 19:11:20 +0530

    Update Enrollment.jsx

--- src/pages/Enrollment.jsx
+++ src/pages/Enrollment.jsx
@@ -16,7 +16,7 @@ const Enrollment = () => {
     }
 
     try {
-      const response = await fetch('http://localhost:5000/api/enroll', {
+      const response = await fetch('https://event-backend-1uul.onrender.com', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
